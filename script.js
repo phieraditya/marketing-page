c40ac5c2f2d460bf52553e8f15527023faf79966
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-01-21 16:13:29 +0700

    implement fade effect on navigation

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ const section1 = document.querySelector('#section--1');
 
 const navLinks = document.querySelector('.nav__links');
 
-const nav = document.querySelector('.nav');
-
 const tabContainer = document.querySelector('.operations__tab-container');
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContent = document.querySelectorAll('.operations__content');
+
+const nav = document.querySelector('.nav');
 
 const header = document.querySelector('.header');
 
@@ -81,6 +81,21 @@ tabContainer.addEventListener('click', function (e) {
 
 /////////////////////////////////////////////////////////////////
 // Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 /////////////////////////////////////////////////////////////////
 // Sticky navigation
