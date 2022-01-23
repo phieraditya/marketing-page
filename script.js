c97a258d9886a24d9691b1eac197de0ab7d919e9
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-01-23 15:48:12 +0700

    implement sticky navigation

--- script.js
+++ script.js
@@ -99,6 +99,21 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 /////////////////////////////////////////////////////////////////
 // Sticky navigation
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
 
 /////////////////////////////////////////////////////////////////
 // Reveal sections
